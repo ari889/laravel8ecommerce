a9b57ac6005107ec91b0cea6cdae31abff78e02b
Arijit Banarjee <arijitbanarjee889@gmail.com>
2022-01-30 07:59:12 +0600

    Solve product image delation problem

--- app/Http/Livewire/Admin/AdminProductConponent.php
+++ app/Http/Livewire/Admin/AdminProductConponent.php
@@ -12,6 +12,24 @@ class AdminProductConponent extends Component
 
     public function deleteProduct($id){
         $product = Product::find($id);
+
+        if($product->image){
+            if(file_exists(public_path('assets/images/products'.'/'.$product->image))){
+                unlink(public_path('assets/images/products'.'/'.$product->image));
+            }
+        }
+
+        if($product->images){
+            $images = explode(',', $product->images);
+            foreach($images as $image){
+                if($image){
+                    if(file_exists(public_path('assets/images/products'.'/'.$image))){
+                        unlink(public_path('assets/images/products'.'/'.$image));
+                    }
+                }
+            }
+        }
+
         $product->delete();
         session()->flash('message', 'Product Deleted!');
     }
