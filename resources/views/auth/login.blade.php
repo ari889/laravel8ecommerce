06f6eeeb2dfdd038e2a6c1492a049d5d62dd6e46
Arijit Banarjee <arijitbanarjee889@gmail.com>
2022-01-30 07:47:17 +0600

    Make header functional on login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
 </x-guest-layout> --}}
 
 
-<x-guest-layout>
+<x-base-layout>
     <main id="main" class="main-site left-sidebar">
 
 		<div class="container">
@@ -96,4 +96,4 @@
 		</div><!--end container-->
 
 	</main>
-</x-guest-layout>
+</x-base-layout>
